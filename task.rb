899aa9ea3ab332e36a5f93dc95f2a3a08530ef3b
satou54 <satou54satou@gmail.com>
2021-01-11 15:00:11 +0900

    Ruby課題(基礎)Q14-16

--- task.rb
+++ task.rb
@@ -121,7 +121,8 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  array =  data.keys
+  p array
 end
 
 def q15
@@ -129,7 +130,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.key?(:age) ? "OK" : "NG"
+  puts data2.key?(:age) ? "OK" : "NG"
 end
 
 def q16
@@ -141,7 +143,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
